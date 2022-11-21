1dcc2e663b6a7aa8c14174f796f89862e7f0c71b
sw9504 <sladewilson9504@gmail.com>
2022-11-21 18:07:16 -0300

    Update some screen functions - pre firebase auth

--- app/src/main/java/com/utn/temptoothlauria/fragments/AvatarFragment.kt
+++ app/src/main/java/com/utn/temptoothlauria/fragments/AvatarFragment.kt
@@ -1,5 +1,8 @@
 package com.utn.temptoothlauria.fragments
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,7 +11,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.utn.temptoothlauria.R
+import com.utn.temptoothlauria.activities.MainActivity
 import com.utn.temptoothlauria.databinding.FragmentAvatarBinding
 import com.utn.temptoothlauria.viewmodels.AvatarViewModel
 
@@ -26,13 +31,37 @@ class AvatarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAvatarBinding.inflate(inflater, container, false)
+
+        // HIde bottom bar
         val view = requireActivity().findViewById<BottomNavigationView>(R.id.bottomBar)
         view.visibility = View.VISIBLE
+        ///
         return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-    }
 
+        binding.btnLogOut.setOnClickListener {
+                // Dialog for Logout confirmation.
+                // Use material design library
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Logout")
+                    .setMessage("Are you sure you want to Logout?")
+                    .setNegativeButton("NO") { dialog, which ->
+                        // Nothing
+                    }
+                    .setPositiveButton("YES") { dialog, which ->
+                        //
+                        // Go to LoginFragment with no back stack
+                        val intent = Intent(activity, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent,null)
+                        activity?.finish()
+                    }
+                    .show()
+        }
+    }
 }
