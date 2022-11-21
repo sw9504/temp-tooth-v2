9ec6c7794adb03c23decc04f977f9939104a40b0
sw9504 <sladewilson9504@gmail.com>
2022-11-21 13:09:30 -0300

    Add coroutines in firestores recyclerview

--- app/src/main/java/com/utn/temptoothlauria/fragments/ListFragment.kt
+++ app/src/main/java/com/utn/temptoothlauria/fragments/ListFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -52,13 +53,14 @@ class ListFragment : Fragment() {
             viewModel.getValueList()
         }
 
-            viewModel.valueList.observe(viewLifecycleOwner, Observer {
-                adapter = ValueAdapter(it) { position ->
+        viewModel.valueList.observe(viewLifecycleOwner, Observer {
+                adapter = ValueAdapter(it!!) { position ->
                     var action = ListFragmentDirections.actionListFragmentToExpandedFragment()
                     findNavController().navigate(action)
                 }
                 binding.recValue.layoutManager = LinearLayoutManager(requireContext())
                 binding.recValue.adapter = adapter
-            })
+        })
+
     }
 }
--- app/src/main/java/com/utn/temptoothlauria/viewmodels/ListViewModel.kt
+++ app/src/main/java/com/utn/temptoothlauria/viewmodels/ListViewModel.kt
@@ -1,37 +1,49 @@
 package com.utn.temptoothlauria.viewmodels
 
-import android.content.ContentValues
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.ktx.toObject
 import com.utn.temptoothlauria.entities.Value
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
 
-var realList : MutableList<Value> = mutableListOf()
+private var COLLECTION_PATH : String = "value"
+private var USER_FIELD : String  = "userId"
+var userId : String = "pedro45"
+private val db = Firebase.firestore
 
 class ListViewModel : ViewModel() {
-    var valueList : MutableLiveData<MutableList<Value>> = MutableLiveData()
-    val db = Firebase.firestore
+    var valueList : MutableLiveData<MutableList<Value>?> = MutableLiveData()
 
     fun getValueList () {
-        db.collection("value")
-            .get()
-            .addOnSuccessListener { values ->
-                valueList.value?.clear()
-                if (values != null) {
-                    for (value in values) {
-                        realList.add(value.toObject<Value>())
-                        // Ordenar por fecha y hora
-                        Log.w("Firestore", "Success! ${value.toObject<Value>()}")
-                    }
-                    valueList.value = realList
-                }
-            }
-            .addOnFailureListener {
-                Log.w("Firestore", "Error getting documents: ")
-            }
+        viewModelScope.launch (Dispatchers.Main) {
+            valueList.value = getDatafromFirestore()
+        }
+    }
+
+    suspend fun getDatafromFirestore() : MutableList<Value> {
+        var realList : MutableList<Value> = mutableListOf()
+        val queryRef = db.collection(COLLECTION_PATH)
+
+         try {
+            val values = queryRef.whereEqualTo(USER_FIELD,userId).get().await()
+             if (values != null) {
+                 for (value in values) {
+                     realList.add(value.toObject<Value>())
+                     // Ordenar por fecha y hora
+                     Log.w("Firestore", "Success! ${value.toObject<Value>()}")
+                 }
+             }
+         }
+         catch (e : Exception) {
+            Log.w("Firestore", "Error getting documents")
+         }
+        return realList
     }
 }
